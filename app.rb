496102720df21742cef9130cc578a7384ec4b910
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-14 21:26:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -51,11 +51,12 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
-    session[:id].clear
+    # session[:id].clear
+    session = {}
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
- 
+
   get '/finish' do
     "Hopefully that explains a little more about the concept of sessions.\nThe session is simply a way to store user data on a temporary basis.\nIn any web application, a user ID is typically used as a session ID.\nThis is because an ID attribute of a user is a unique identifier\nthat will always be distinguishable from other user ID attributes."
   end
